a801cd176a42181c14cc7dfd5df1b56712c857ac
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-09-03 11:35:52 +0900

    implement Todos() but compile error

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -18,7 +18,11 @@ func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) 
 
 // Todos is the resolver for the todos field.
 func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: Todos - todos"))
+	todo1 := model.Todo{ID: "1", Text: "Go shopping", Done: false}
+	todo2 := model.Todo{ID: "2", Text: "Finish homework", Done: true}
+	todo3 := model.Todo{ID: "3", Text: "Squat 100 reps", Done: false}
+
+	return []*model.Todo{todo1, todo2, todo3}, nil
 }
 
 // Mutation returns MutationResolver implementation.
